7ba7124051d0ced46bae4a2667becb5088ddaa41
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-09-18 23:06:40 +0800

    ✨ feat(demos): 新增 demo

---
+++ source/demos/work-hns5j/TableCards/precache-manifest.be60f2cce8740408f49c567475e6f067.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed9014dd7bafa8438928",
+    "url": "css/app.d88aee2b.css"
+  },
+  {
+    "revision": "ed9014dd7bafa8438928",
+    "url": "js/app.c9a80afe.js"
+  },
+  {
+    "revision": "1e3926d6ef0c268b08a5",
+    "url": "css/chunk-vendors.ba55eb9d.css"
+  },
+  {
+    "revision": "1e3926d6ef0c268b08a5",
+    "url": "js/chunk-vendors.5483ff8f.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "fc24b29bfdf517f10cf5443bbea002b7",
+    "url": "index.html"
+  },
+  {
+    "revision": "be5aed352c6721fd1a5d641c800da855",
+    "url": "api/tableCard/last"
+  },
+  {
+    "revision": "c1eacbde521947a1566015589e8c8f7a",
+    "url": "t.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
